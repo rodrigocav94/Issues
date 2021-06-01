872a6894edb3278c64900db7699c871c1d39bbea
Rodrigo Alceu Cavalcanti <rodrigocav94@gmail.com>
2021-06-01 15:55:26 -0300

    Initial Commit

---
+++ Issues/IssuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IssuesApp.swift
+//  Issues
+//
+//  Created by Rodrigo Cavalcanti on 01/06/21.
+//
+
+import SwiftUI
+
+@main
+struct IssuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
